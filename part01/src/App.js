0993bf672e2b2a2a9b0f84863b6824916d91fb23
FededelRincon <delrincon@gmail.com>
2021-02-13 22:30:30 -0300

    first commit from console

---
+++ part01/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Mensaje from './Mensaje';
+
+
+const Description = () => {
+  return <p>Esta es la app del curso fullstack bootcamp</p>
+}
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Mensaje color='red' message='Estamos trabajando' />
+      <Mensaje color='green' message='En un curso'/>
+      <Mensaje color='orange' message='De React'/>
+      <Description />
+    </div>
+  );
+}
+
+export default App;
